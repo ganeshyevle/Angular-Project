5667a5cd5ec13f1e25664fbcdcda8125c3b01c54
Ganesh Yevle <ganeshyevle9011@gmail.com>
2023-11-07 19:42:01 +0530

    Files Added

--- src/app/mainbody/mainbody.component.ts
+++ src/app/mainbody/mainbody.component.ts
@@ -13,7 +13,7 @@ export class MainbodyComponent {
       chores: ['Taking out the trash when its full', 'Taking the trash out for pickup if required.'],
     },
     {
-      title: 'GUARDIANS OF THE GALAXY',
+      title: ' Guardians of the Galaxy Vol. 3',
       imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4bXY4exM8dLa5dT9jlxCanlHHYdQUOK-lSw&usqp=CAU',
       chores: ['Taking out the trash when its full', 'Taking the trash out for pickup if required.'],
     },
@@ -22,26 +22,28 @@ export class MainbodyComponent {
       imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMbte8TXJVVgyUQeJ6hFymzdS9l0cBGVhh1g&usqp=CAU',
       chores: ['Sweeping the kitchen floor', 'wiping down countertops.'],
     },
+    {
+      title: 'Oppenheimer (I)',
+     
+    },
+    {
+      title: 'Spider-Man: Across the Spider-Verse',
+   
+    },
+    {
+      title: 'Mission: Impossible - Dead Reckoning Part One',
     
+    },
+    {
+      title: ' Dungeons & Dragons: Honor Among Thieves',
+     
+    },
+    {
+      title: ' Aquaman and the Lost Kingdom',
+      
+    },
     
   ];
-  movies2: any[] = [
-    {
-      title: 'The Avengers ',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDPCGTMVY07hgEM3ied7pc7q1HxPzWGCv0Lw&usqp=CAU',
-      chores: ['Taking out the trash when its full', 'Taking the trash out for pickup if required.'],
-    },
-    {
-      title: 'GUARDIANS OF THE GALAXY',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4bXY4exM8dLa5dT9jlxCanlHHYdQUOK-lSw&usqp=CAU',
-      chores: ['Taking out the trash when its full', 'Taking the trash out for pickup if required.'],
-    },
-    {
-      title: 'Tiger 3',
-      imageUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMbte8TXJVVgyUQeJ6hFymzdS9l0cBGVhh1g&usqp=CAU',
-      chores: ['Sweeping the kitchen floor', 'wiping down countertops.'],
-    },
-    
-    
-  ];
+
+
 }
